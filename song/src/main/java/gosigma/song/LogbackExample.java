66f38bbba01826a30e2b01135e0920e7819c0b72
Yongjian Xi <xiyongjian@hotmail.com>
2017-10-31 21:41:29 -0600

    htmlunit + jsoup for html/xml parsing, with logback logging setup

--- song/src/main/java/gosigma/song/LogbackExample.java
+++ song/src/main/java/gosigma/song/LogbackExample.java
@@ -7,23 +7,29 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.StatusPrinter;
 
 public class LogbackExample {
+	// usage : before class initialization
+	static {
+		System.setProperty("logback.configurationFile", "/path/to/config.xml");
+	}
+
 	final static Logger logger = LoggerFactory.getLogger(LogbackExample.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        logger.info("Msg #1");
-        logger.warn("Msg #2");
-        logger.error("Msg #3");
-        logger.debug("Msg #4");
-        
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        // print logback's internal status
-        StatusPrinter.print(lc);
+		// LoggerContext lc = (LoggerConSystem.setProperty("logback.configurationFile",
+		// "/path/to/config.xml");text) LoggerFactory.getILoggerFactory();
+		// // print logback's internal status
+		// StatusPrinter.print(lc);
 
-		final String dir = System.getProperty("user.dir");
-		System.out.println("[current dir] - " + dir);
-		String classpathStr = System.getProperty("java.class.path");
-		System.out.println("[classpath] - " + classpathStr.replaceAll(";",  "\n"));
+		// TODO Auto-generated method stub
+		logger.info("Msg #1");
+		logger.warn("Msg #2");
+		logger.error("Msg #3");
+		logger.debug("Msg #4");
+
+		// final String dir = System.getProperty("user.dir");
+		// System.out.println("[current dir] - " + dir);
+		// String classpathStr = System.getProperty("java.class.path");
+		// System.out.println("[classpath] - " + classpathStr.replaceAll(";", "\n"));
 	}
 
 }
