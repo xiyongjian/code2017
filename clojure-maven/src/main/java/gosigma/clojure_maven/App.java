6a9f87730668d978e3aec52e80719b9b11cbf030
Yongjian Xi <xiyongjian@hotmail.com>
2017-11-05 16:32:24 -0700

    more clojure and kotlin

---
+++ clojure-maven/src/main/java/gosigma/clojure_maven/App.java
@@ -0,0 +1,13 @@
+package gosigma.clojure_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
