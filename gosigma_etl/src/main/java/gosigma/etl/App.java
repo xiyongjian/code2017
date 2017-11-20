215b4ed501621dfb864b86dfcb9d4f8ac0a8d4bc
Yongjian Xi <xiyongjian@hotmail.com>
2017-11-19 22:02:03 -0700

    intial

---
+++ gosigma_etl/src/main/java/gosigma/etl/App.java
@@ -0,0 +1,13 @@
+package gosigma.etl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
